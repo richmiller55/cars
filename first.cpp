b8b7dbec6d898a11fb65caaed2c2030eabf7f037
richmiller55 <rich@richmiller.com>
2021-03-10 02:21:08 +0000

    little file io

--- first.cpp
+++ first.cpp
@@ -1,10 +1,22 @@
 // first.cpp
+#include <fstream>
+#include <string>
 #include <iostream>
 
 using namespace std;
- 
+
+int readdata() {
+  string str;
+  ifstream wheels ( "/home/ec2-user/data/batch001/Wheels.csv" );
+  while (getline (wheels, str)) {
+    cout << str << "\n";
+  }
+}
+
 int main() {
+  readdata();
   cout << "project to read beam data raw" << endl;
    return 0;
 }
+
 
